55e4f686156a68dec5f0e4676e74390ffa620c67
Jordan Klassen <jordan@klassen.me.uk>
2015-08-14 01:22:02 -0700

    Implement line separator escaping

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ var buffers = {
     escapeF: Buffer('\\f'),
     escapeN: Buffer('\\n'),
     escapeR: Buffer('\\r'),
-    escapeT: Buffer('\\t')
+    escapeT: Buffer('\\t'),
+    escapeLineSeparator: Buffer('\\u2028'),
+    escapeParagraphSeparator: Buffer('\\u2029')
 };
 
 for (var i = 0; i < 32; i++) {
@@ -24,6 +26,14 @@ var codes = {
     r: '\r'.charCodeAt(0),
     t: '\t'.charCodeAt(0)
 };
+
+var multiByteBuffers = {
+    lineSeparator: Buffer('\u2028', 'utf8'),
+    paragraphSeparator: Buffer('\u2029', 'utf8')
+};
+var multiByteSeparatorLength = multiByteBuffers.lineSeparator.length; // same for both
+var multiByteSeparatorOffset = multiByteSeparatorLength - 1;
+var multiByteSeparatorCode = multiByteBuffers.lineSeparator[0]; // same for both
 
 var map = {};
 map[codes.quote] = buffers.escapeQuote;
@@ -53,6 +63,21 @@ module.exports = function () {
                 var bufs = [ buf.slice(offset, i), buffers[c] ];
                 this.push(Buffer.concat(bufs));
                 offset = i + 1;
+            }
+            else if (c === multiByteSeparatorCode) {
+                var rawBuf = buf.slice(i, i + multiByteSeparatorLength);
+                var escapeBuf;
+                if (rawBuf.equals(multiByteBuffers.lineSeparator)) {
+                  escapeBuf = buffers.escapeLineSeparator;
+                } else if (rawBuf.equals(multiByteBuffers.paragraphSeparator)) {
+                  escapeBuf = buffers.escapeParagraphSeparator;
+                }
+                if (escapeBuf) {
+                    var bufs = [ buf.slice(offset, i), escapeBuf ];
+                    this.push(Buffer.concat(bufs));
+                    offset = i + multiByteSeparatorLength;
+                    i += multiByteSeparatorOffset
+                }
             }
         }
         if (offset === 0) this.push(buf)
--- test/unicode_separators.js
+++ test/unicode_separators.js
@@ -2,10 +2,20 @@ var quote = require('../');
 var concat = require('concat-stream');
 var test = require('tape');
 
-test('whitespace', function (t) {
+test('js string unicode separators', function (t) {
     t.plan(1);
     var q = quote();
     q.end('beep\u2028ima\u2029jeep');
+
+    q.pipe(concat(function (body) {
+        t.equal(body.toString('utf8'), '"beep\\u2028ima\\u2029jeep"');
+    }));
+});
+
+test('utf8 separators', function (t) {
+    t.plan(1);
+    var q = quote();
+    q.end(Buffer('beep\u2028ima\u2029jeep', 'utf8'));
 
     q.pipe(concat(function (body) {
         t.equal(body.toString('utf8'), '"beep\\u2028ima\\u2029jeep"');
